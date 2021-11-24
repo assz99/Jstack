ccf48eb4fab67a16c9173455c6dddbdd54025654
Renan <renanguides@hotmail.com>
2021-11-24 19:23:58 -0400

    Query Params and URL Params
    Adding  treatment for Query Params and URL Params

--- firstApi/src/routes.js
+++ firstApi/src/routes.js
@@ -6,5 +6,9 @@ module.exports = [
     endpoint:'/users',
     method:'GET',
     handler: UserController.listUsers,
+  },{
+    endpoint:'/users:id',
+    method:'GET',
+    handler: UserController.getUserById,
   },
 ];
