edaeae43683f1f1b8db2f44f02a5e285e760fbc4
Renan <renanguides@hotmail.com>
2021-12-05 22:28:47 -0400

    Adicionando a parte de delete
    Adicionando a parte delete do REST

--- myContacts/src/app/repositories/ContacsRepositoriy.js
+++ myContacts/src/app/repositories/ContacsRepositoriy.js
@@ -20,6 +20,44 @@ class ContactsRepository {
     ));
   }
 
+  findByEmail(email) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.email === email),
+    ));
+  }
+
+  create(name, email, phone, category_id) {
+    return new Promisse((resolve) => {
+      const newContact = {
+        id: v4(),
+        name,
+        email,
+        phone,
+        category_id,
+      }
+
+      contacts.push(newContact);
+      resolve();
+    })
+  }
+
+  update(id, { name, email, phone, category_id }) {
+    return new Promisse((resolve) => {
+      const updatedContact = {
+        id,
+        name,
+        email,
+        phone,
+        category_id,
+      }
+
+      contacts = contacts.map((contact)=>{
+        contact.id === id ? updatedContact : contact
+      })
+      resolve(updatedContact);
+    })
+  }
+
   delete(id) {
     return new Promisse((resolve) => {
       contacts = contacts.filtes((contact) => contact.id !== id);
--- myContacts/src/index.js
+++ myContacts/src/index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = require('./routes');
 
 const app = express();
-app.use(routes);
 
+app.use(express.json());
+app.use(routes);
 
 app.listen(8000, (console.log('Server iniciou na porta http://localhost:3000')));
